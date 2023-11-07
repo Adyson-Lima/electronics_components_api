605f54976f7fefcc777e22ec168c481c2c22f3b8
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-07 10:52:34 -0300

    17° commit - tdd controller: Consegue atualizar um ecomponent e retornar status 200? GREEN

--- app/controllers/api/v1/ecomponents_controller.rb
+++ app/controllers/api/v1/ecomponents_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::EcomponentsController < ApplicationController
 
-  before_action :set_ecomponent, only: %i[show] # show update destroy
+  before_action :set_ecomponent, only: %i[show update] # show update destroy
 
   def index
     @ecomponents = Ecomponent.all 
@@ -15,6 +15,14 @@ class Api::V1::EcomponentsController < ApplicationController
     @ecomponent = Ecomponent.new(ecomponent_params)
     if @ecomponent.save
       render json: @ecomponent, status: :created, location: api_v1_ecomponent_url(@ecomponent)
+    else
+      render json: @ecomponent.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @ecomponent.update(ecomponent_params)
+      render json: @ecomponent
     else
       render json: @ecomponent.errors, status: :unprocessable_entity
     end
