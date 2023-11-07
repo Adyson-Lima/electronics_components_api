fd2497155970023d8688f1940bb9fac929ae1e8f
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-07 10:30:34 -0300

    12° commit - tdd controller: Consegue retornar um ecomponent especifico e retornar status 200? RED

--- spec/controllers/v1/ecomponents_controller_v1_spec.rb
+++ spec/controllers/v1/ecomponents_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::EcomponentsController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/ecomponents/id' do
+    it 'Consegue retornar um ecomponent especifico e retornar status 200?' do
+      get :show, params: {id: @ecomponent.id}
+      expect(response.body).to include_json(id: @ecomponent.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
